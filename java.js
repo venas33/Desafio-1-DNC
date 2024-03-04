f6e5ad81d4b95e21a2dceebe98e4ace634ebec7f
venas33 <venanciofasb@gmail.com>
2024-03-04 12:02:27 -0300

    Versao Inicial

---
+++ java.js
@@ -0,0 +1,20 @@
+const box = document.getElementById("box");
+const img = document.querySelector("img");
+
+box.addEventListener("mousemove", (e) =>{
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offserTop;
+
+    console.log(x, y);
+
+    img.style.transformOrigin = `${x}px ${y}px`;
+    img.style.transform = "scale(1.2)";
+});
+
+box.addEventListener("mouseleave", () => {
+
+    img.style.transformOrigin = "center center";
+    img.style.transform = "scale(1)";
+
+
+})
